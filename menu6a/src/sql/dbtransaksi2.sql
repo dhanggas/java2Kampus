06e84b88ee24803b6f2edcf6fab8a4269785932a
dhanggas <dhanggas@gmail.com>
2018-05-15 19:28:54 +0700

    panggil laporan

---
+++ menu6a/src/sql/dbtransaksi2.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2018 at 02:28 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbtransaksi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbbarang`
+--
+
+CREATE TABLE `tbbarang` (
+  `kode` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jenis` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbbarang`
+--
+
+INSERT INTO `tbbarang` (`kode`, `nama`, `jenis`) VALUES
+('A01', 'Net Beans', 'Data');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbjabatan`
+--
+
+CREATE TABLE `tbjabatan` (
+  `kdjab` varchar(5) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `tunjangan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbjabatan`
+--
+
+INSERT INTO `tbjabatan` (`kdjab`, `jabatan`, `tunjangan`) VALUES
+('J01', 'admin', 600000),
+('J02', 'operator', 700000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbpegawai`
+--
+
+CREATE TABLE `tbpegawai` (
+  `nip` varchar(3) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `gpokok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbpegawai`
+--
+
+INSERT INTO `tbpegawai` (`nip`, `nama`, `gpokok`) VALUES
+('P01', 'jono', 10000),
+('P02', 'roni', 50000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbtransaksi`
+--
+
+CREATE TABLE `tbtransaksi` (
+  `nip` varchar(5) NOT NULL,
+  `kdjab` varchar(5) NOT NULL,
+  `overtime` int(11) NOT NULL,
+  `gajibersih` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbtransaksi`
+--
+
+INSERT INTO `tbtransaksi` (`nip`, `kdjab`, `overtime`, `gajibersih`) VALUES
+('P01', 'J01', 300000, 910000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `kode` varchar(5) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`kode`, `nama`, `harga`) VALUES
+('A01', 'Pulpen', 2000),
+('A02', 'Penghapus', 1500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_laporan`
+--
+
+CREATE TABLE `tb_laporan` (
+  `id` varchar(5) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_laporan`
+--
+
+INSERT INTO `tb_laporan` (`id`, `nama`, `harga`, `jumlah`) VALUES
+('1', 'Pulpen', 5000, 5),
+('2', 'Sarung', 2000, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `kode` varchar(5) NOT NULL,
+  `kurir` varchar(50) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`kode`, `kurir`, `jumlah`, `harga`) VALUES
+('A01', 'jono', 5, 10000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbbarang`
+--
+ALTER TABLE `tbbarang`
+  ADD PRIMARY KEY (`kode`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
